97700160387332a19df83837922376856b9be409
Maran Filippo <filippo.maran.fm91@gmail.com>
2019-11-15 16:16:13 +0100

    fatto javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+var listaMail = ['pippo@gmail.it', 'paperino@gmail.it', 'topolino@gmail.it']
+console.log(listaMail);
+var mailUtente = prompt('Qual è la tua mail?');
+console.log(mailUtente);
+for (var i = 0; i < listaMail.length; i++) {
+    var elementiLista = listaMail[i]
+    if (mailUtente == elementiLista) {
+        console.log('Benvenuto');
+        document.getElementById('testo').innerHTML = "Benvenuto";
+    } else {
+        console.log('Indirizzo non trovato')
+        document.getElementById('testo').innerHTML = "Accesso negato";
+    }
+
+}
