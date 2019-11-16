8587d6b60c7054fc9f69288c65fb9b7ede2e61ed
Maran Filippo <filippo.maran.fm91@gmail.com>
2019-11-16 15:56:07 +0100

    il check dava problemi in quanto il ciclo continuava fino alla fine, trovato e aggiunto comando break nell' if, in caso la mail utente combaciasse con mail in array

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var listaMail = ['pippo@gmail.it', 'paperino@gmail.it', 'topolino@gmail.it']
+var listaMail = ['pippo@gmail.it', 'paperino@gmail.it', 'topolino@gmail.it'];
 console.log(listaMail);
 var mailUtente = prompt('Qual è la tua mail?');
 console.log(mailUtente);
 for (var i = 0; i < listaMail.length; i++) {
-    var elementiLista = listaMail[i]
+    var elementiLista = listaMail[i];
     if (mailUtente == elementiLista) {
         console.log('Benvenuto');
         document.getElementById('testo').innerHTML = "Benvenuto";
-    } else {
+        break;
+    }
+    else {
         console.log('Indirizzo non trovato')
         document.getElementById('testo').innerHTML = "Accesso negato";
     }
